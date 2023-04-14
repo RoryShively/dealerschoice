ee09a390617f54e21dcd61e1aae0a7a6f89847bc
RoryShively <roryshively1@gmail.com>
2023-04-13 19:10:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Dealers Chioce
+
+This is a silly small utility that allows you to enter various choices via a command prompt and have one chosen at random for you.
+
+Feature list:
+- create actions such as add or remove item from choices
+- dedup choices
+- cleanup output
+
+
+Example use (Will clean this up when actions feature is completed):
+bash```
+Enter new choice (type "done" to exit):
+d
+input: d
+len(input): 2
+choices: ["d"]
+len(choices): 1
+exit_loop: false
+Enter new choice (type "done" to exit):
+e
+input: e
+len(input): 2
+choices: ["d", "e"]
+len(choices): 2
+exit_loop: false
+Enter new choice (type "done" to exit):
+c
+input: c
+len(input): 2
+choices: ["d", "e", "c"]
+len(choices): 3
+exit_loop: false
+Enter new choice (type "done" to exit):
+done
+input: done
+len(input): 5
+Some("c")
+```
---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use rand::seq::SliceRandom;
+
+/*
+
+feature list:
+- create actions such as add or remove item from choices
+- dedup choices
+- cleanup output
+
+ */
+
+fn main() {
+    let mut choices: Vec<String> = Vec::new();
+
+    loop {
+        let mut input = String::new();
+        println!("Enter new choice (type \"done\" to exit):");
+        let b1 = std::io::stdin().read_line(&mut input).unwrap();
+        input.pop();  // remove last char (\n)
+
+        println!("input: {}", input);
+        println!("len(input): {}", b1);
+
+        let exit_loop = input.eq("done");
+        
+        if exit_loop {
+            break
+        } else {
+            choices.push(input.clone());
+
+            println!("choices: {:?}", choices);
+            println!("len(choices): {}", choices.len());
+            println!("exit_loop: {}", exit_loop);
+        } 
+    }
+
+    println!("{:?}", choices.choose(&mut rand::thread_rng()));
+}
