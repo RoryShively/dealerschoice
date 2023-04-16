44322ad856b00fde93072f1c4bcbc3e708eb1804
RoryShively <roryshively1@gmail.com>
2023-04-15 19:32:31 -0500

    added actions, split into modules, cleaned up cmd line output

--- README.md
+++ README.md
@@ -3,39 +3,38 @@
 This is a silly small utility that allows you to enter various choices via a command prompt and have one chosen at random for you.
 
 TODO feature list:
-- create actions such as add or remove item from choices
 - dedup choices
-- cleanup output
 - add testing
-- split into modules
 - better error handling
 
-Example use (Will clean this up when actions feature is completed):
+Example use:
 ```
-Enter new choice (type "done" to exit):
-d
-input: d
-len(input): 2
-choices: ["d"]
-len(choices): 1
-exit_loop: false
-Enter new choice (type "done" to exit):
-e
-input: e
-len(input): 2
-choices: ["d", "e"]
-len(choices): 2
-exit_loop: false
-Enter new choice (type "done" to exit):
-c
-input: c
-len(input): 2
-choices: ["d", "e", "c"]
-len(choices): 3
-exit_loop: false
-Enter new choice (type "done" to exit):
-done
-input: done
-len(input): 5
-Some("c")
+Current choices: []
+Chose which action you would like to perform
+    Enter number of action you want (ex: 1, 2, 3)
+1) Add an element to choices
+2) Remove an element to choices
+3) Randomly select an choice
+
+1
+Enter new choice:
+get x
+
+Current choices: ["get x"]
+Chose which action you would like to perform
+    Enter number of action you want (ex: 1, 2, 3)
+1) Add an element to choices
+2) Remove an element to choices
+3) Randomly select an choice
+
+1
+Enter new choice:
+get y
+
+Current choices: ["get x", "get y"]
+Chose which action you would like to perform
+    Enter number of action you want (ex: 1, 2, 3)
+1) Add an element to choices
+2) Remove an element to choices
+3) Randomly select an choice
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
 use rand::seq::SliceRandom;
 
+pub mod actions;
+
+// I thoroughly researched the seemingly trivial desicion to declare this
+// as either a static or const str since either would work. Details on the
+// difference can be found at 
+// https://rust-lang.github.io/rfcs/0246-const-vs-static.html
+// 
+// I chose const becuase it inlines the str into code instead of needing to
+// reference an address as static would
+const ACTION_DIALOG_PROMPT: &str = r#"Chose which action you would like to perform
+    Enter number of action you want (ex: 1, 2, 3)
+1) Add an element to choices
+2) Remove an element to choices
+3) Randomly select an choice
+"#;
+
+
 fn main() {
+    // let mut choices: Vec<String> = Vec::new();
     let mut choices: Vec<String> = Vec::new();
 
+
     loop {
+        println!("\nCurrent choices: {:?}", choices);
+
         let mut input = String::new();
-        println!("Enter new choice (type \"done\" to exit):");
-        let b1 = std::io::stdin().read_line(&mut input).unwrap();
+        println!("{}", ACTION_DIALOG_PROMPT);
+        // use expect instead of unwrap
+        // https://users.rust-lang.org/t/how-to-read-an-integer-from-stdin/57538
+        let _ = std::io::stdin().read_line(&mut input).unwrap();
         input.pop();  // remove last char (\n)
 
-        println!("input: {}", input);
-        println!("len(input): {}", b1);
-
-        let exit_loop = input.eq("done");
-        
-        if exit_loop {
-            break
-        } else {
-            choices.push(input.clone());
-
-            println!("choices: {:?}", choices);
-            println!("len(choices): {}", choices.len());
-            println!("exit_loop: {}", exit_loop);
-        } 
+        match input.as_str() {
+            "1" => actions::add::add(&mut choices),
+            "2" => println!("remove"),
+            "3" => {
+                println!("select");
+                break;
+            },
+            _ => println!("incorrect input submitted"),
+        }
     }
 
-    println!("{:?}", choices.choose(&mut rand::thread_rng()));
+    // println!("{:?}", choices.choose(&mut rand::thread_rng()));
 }
